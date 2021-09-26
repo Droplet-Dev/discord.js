f24ab2e8da488276f41a9759db6d266d456de990
Erik <44114242+MrErikCodes@users.noreply.github.com>
2021-09-26 18:28:06 +0200

    Stop the cache on messages

--- src/client/actions/MessageCreate.js
+++ src/client/actions/MessageCreate.js
@@ -7,7 +7,7 @@ let deprecationEmitted = false;
 
 class MessageCreateAction extends Action {
   handle(data) {
-    const client = this.client;
+   /* const client = this.client;
     const channel = this.getChannel(data);
     if (channel) {
       const existing = channel.messages.cache.get(data.id);
@@ -15,26 +15,11 @@ class MessageCreateAction extends Action {
       const message = channel.messages._add(data);
       channel.lastMessageId = data.id;
 
-      /**
-       * Emitted whenever a message is created.
-       * @event Client#messageCreate
-       * @param {Message} message The created message
-       */
       client.emit(Events.MESSAGE_CREATE, message);
 
-      /**
-       * Emitted whenever a message is created.
-       * @event Client#message
-       * @param {Message} message The created message
-       * @deprecated Use {@link Client#messageCreate} instead
-       */
-      if (client.emit('message', message) && !deprecationEmitted) {
-        deprecationEmitted = true;
-        process.emitWarning('The message event is deprecated. Use messageCreate instead', 'DeprecationWarning');
-      }
-
+     
       return { message };
-    }
+    }*/
 
     return {};
   }
